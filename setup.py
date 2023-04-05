9c846128f859f4934a9fa07ddc87065e0ba5ce85
Simon Vary <7817718+simonvary@users.noreply.github.com>
2023-04-05 13:36:36 +0200

    Added matplotlib, geotorch to required packages

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 
 setup(name='landing_stiefel',
-      install_requires=['torch', 'geoopt', 'scipy'],
+      install_requires=['torch', 'geoopt', 'scipy', 'geotorch', 'matplotlib'],
       packages=['landing_stiefel'],
       version='0.0'
       )
